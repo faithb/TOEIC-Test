bc60b664e22c518313d1888bb745fd8dec9fba6f
faithb <tranvanhung10091996@gmail.com>
2019-04-23 17:20:01 +0700

    fix button setting

--- app/src/main/java/vn/asiantech/englishtest/MainActivity.kt
+++ app/src/main/java/vn/asiantech/englishtest/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         window.statusBarColor = resources.getColor(R.color.colorBlue)
-        window.closeAllPanels()
         screenDelay()
     }
 
